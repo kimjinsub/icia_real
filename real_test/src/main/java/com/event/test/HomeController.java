c66f99e6f3cf2a04a6921621d5d0afa2e7e1b7f3
jinsub <kimjinsub01@naver.com>
2019-05-28 15:47:48 +0900

    다같이0528 15:47

--- real_test/src/main/java/com/event/test/HomeController.java
+++ real_test/src/main/java/com/event/test/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
@@ -18,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+	ModelAndView mav;
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
@@ -36,4 +37,14 @@ public class HomeController {
 		return "home";
 	}
 	
+	@RequestMapping(value = "/joinFrm", method = RequestMethod.GET)
+	public String joinFrm() {
+		return "joinFrm";
+	}
+	
+	@RequestMapping(value = "/memberInsert", method = RequestMethod.GET)
+	public ModelAndView memberInsert() {
+		mav=new ModelAndView();
+		return mav;
+	}
 }
---
+++ real_test/src/main/webapp/WEB-INF/views/joinFrm.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="memberInsert">
+	<input type="text" name="id" placeholder="아이디를 입력하세요"/>
+	<input type="password" name="pw" placeholder="비밀번호를 입력하세요"/>
+	<input type="text" name="name" placeholder="이름을 입력하세요"/>
+	<button>가입하기</button>
+</form>
+</body>
+</html>
